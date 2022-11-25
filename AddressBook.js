61d6659735020801805119960f23750ffbde648f
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-25 10:19:25 +0530

    Added:Create AddressBookTable

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,26 @@
-console.log("Welcome To The AddressBook");
+class AddressBook {
+
+	Constructor ( firstName,  lastName, phoneNumber,  address
+			, pincode,  email) {
+
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.phoneNumber=phoneNumber;
+		this.address=address;
+		this.pincode=pincode;
+		this.email=email;
+	}
+    newAddressBookDataEnter(){
+        console.log(firstName+"  "+lastName+"  "+phoneNumber+"  "+address+"  "+pincode+"  "+email);
+    }
+}
+const prompt=require("prompt-sync")({sigint:true});
+var firstName=prompt("Enter FirstName  ");
+var lastName=prompt("Enter LastName  "); 
+var phoneNumber =prompt("Enter Number  ");
+var address=prompt("Enter Address  ");
+var pincode =prompt("Enter pincode  ");
+var email=prompt("Enter Email  ");
+var obj=new AddressBook(firstName,  lastName, phoneNumber,  address
+    , pincode,  email);
+    obj.newAddressBookDataEnter();
