e88aabd9b4874d625ecbfbf62b6c0efd356ec78d
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-25 10:38:37 +0530

    Refactor:Ensure That Enteries Are Valid Input

--- AddressBook.js
+++ AddressBook.js
@@ -1,26 +1,86 @@
 class AddressBook {
 
-	Constructor ( firstName,  lastName, phoneNumber,  address
-			, pincode,  email) {
-
-		this.firstName=firstName;
-		this.lastName=lastName;
-		this.phoneNumber=phoneNumber;
-		this.address=address;
-		this.pincode=pincode;
-		this.email=email;
-	}
     newAddressBookDataEnter(){
-        console.log(firstName+"  "+lastName+"  "+phoneNumber+"  "+address+"  "+pincode+"  "+email);
+        const prompt=require("prompt-sync")({sigint:true});
+try{
+    var firstName=prompt("Enter FirstName  ");
+this.regexPatternForName(firstName);
+var lastName=prompt("Enter LastName  "); 
+this.regexPatternForName(lastName);
+var phoneNumber =prompt("Enter Number  ");
+this.regexPatternMobile(phoneNumber);
+var address=prompt("Enter Address  ");
+this.regexPatternForAddress_State(address);
+var pincode =prompt("Enter pincode  ");
+this.regexPatternPinCode(pincode);
+var email=prompt("Enter Email  ");
+this.regexPatternForEmail(email);
+console.log(firstName+"  "+lastName+"  "+phoneNumber+"  "+address+"  "+pincode+"  "+email);
+}
+catch(err){
+    console.log(err);
+
+}       
+}
+
+    regexPatternForName(pattern){
+        let nameRegex = /^[A-Z]{1}[a-zA-Z]{2,}$/;
+        if(nameRegex.test(pattern))
+         {
+            console.log("true");
+         }else
+         {
+            console.log("false");
+            throw "Enter Valid Name";
+          }
+        }
+        regexPatternForAddress_State(pattern){
+            let addresRegex = /^[A-Z]{1}[a-zA-Z]{3,}$/;
+            if(addresRegex.test(pattern))
+             {
+                console.log("true");
+             }else
+             {
+                console.log("false");
+                throw "Enter Valid Address";
+              }
+            }
+    
+
+         regexPatternMobile(pattern){
+            let pinCodeRegex = /^[0-9]{10}$/;
+            if(pinCodeRegex.test(pattern))
+             {
+                console.log("true");
+             }else
+             {
+                 console.log("false");
+                throw "Enter Valid Number";
+              }
+            }
+            regexPatternPinCode(pattern){
+                let pinCodeRegex = /^[0-9]{6}$/;
+                if(pinCodeRegex.test(pattern))
+                 {
+                    console.log("true");
+                 }else
+                 {
+                    console.log("false");
+                    throw "Enter Valid Pincode";
+                  }
+                }
+    regexPatternForEmail(pattern){
+           let pinCodeRegex = /^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$/;
+           if(pinCodeRegex.test(pattern))
+           {
+              console.log("true");
+            }else
+           {
+            console.log("false");
+            throw "Enter Valid Email";
+           }
     }
 }
-const prompt=require("prompt-sync")({sigint:true});
-var firstName=prompt("Enter FirstName  ");
-var lastName=prompt("Enter LastName  "); 
-var phoneNumber =prompt("Enter Number  ");
-var address=prompt("Enter Address  ");
-var pincode =prompt("Enter pincode  ");
-var email=prompt("Enter Email  ");
-var obj=new AddressBook(firstName,  lastName, phoneNumber,  address
-    , pincode,  email);
+
+var obj=new AddressBook();
     obj.newAddressBookDataEnter();
